0ed00a8fa0728a2491bec2010f34ba8359cb98b8
fazlerabbi13 <mohammad0132794@gmail.com>
2023-08-15 21:49:40 +0600

    first commit

---
+++ newstylejjs/new.js
@@ -0,0 +1,8 @@
+const sections = document.querySelectorAll('section');
+for(const section of sections){
+    section.style.border ='2px solid yellow';
+    section.style.marginTop ='5px';
+    section.style.borderRadius ='12px';
+    section.style.paddingLeft ='10px ';
+    section.style.backgroundColor ='lightGrey'
+}
